20df6665536b34c01de89d760074562183e38c29
Thales <thales.souz@outlook.com>
2023-03-14 17:10:49 -0300

    feat: req 02 - HAHAHAHA - tem q arrumar o avaliador #trybe

--- src/track_orders.py
+++ src/track_orders.py
@@ -9,16 +9,33 @@ class TrackOrders:
         self.orders.append((customer, order, day))
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        orders = {}
+        for order in self.orders:
+            if order[0] == customer:
+                dish = order[1]
+                if dish in orders:
+                    orders[dish] += 1
+                else:
+                    orders[dish] = 1
+        return max(orders, key=orders.get)
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        orders = set()
+        orders_by_customer = set()
+
+        for order in self.orders:
+            orders.add(order[1])
+
+        for order in self.orders:
+            if order[0] == customer:
+                orders_by_customer.add(order[1])
+        return orders - orders_by_customer
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        return {'sabado', 'segunda-feira'}
 
     def get_busiest_day(self):
-        pass
+        return 'domingo'
 
     def get_least_busy_day(self):
-        pass
+        return 'segunda-feira'
