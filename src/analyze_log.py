e966304822d979ec3365adacec789393abb5a871
Thales <thales.souz@outlook.com>
2023-03-14 14:48:41 -0300

    feat: implementado excessões de erros

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,11 @@
 def analyze_log(path_to_file):
-    pass
+    if not path_to_file:
+        raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
+    if not path_to_file.endswith(".csv"):
+        raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
+
+    try:
+        with open(path_to_file, "r") as file:
+            print(file)
+    except FileNotFoundError:
+        pass
